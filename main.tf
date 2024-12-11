b466e8aa58c93183a70b5fa1b14b669ef3cb607a
cheedella  Harshath <132482075+Harshath14@users.noreply.github.com>
2024-12-11 11:33:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "aws_instance" "wordpress" {
   security_groups = [aws_security_group.wordpress_sg.name]
 }
 
-resource "aws_security_group" "wordpress_sg" {
+resource "aws_security_group" "wordpress_sg1" {
   name        = "wordpress-sg"
   description = "Allow HTTP, HTTPS, and SSH traffic"
 
